6b021afe1a8f02c170318d85e077fca419db271d
tien96ng <tien96ng@gmail.com>
2021-05-05 15:16:23 -0700

    Add Material UI headless component for styling in Nav.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
 import '../css/App.css';
+
 
 function App() {
   return (
     <>
+      <Nav />
       <h1>Hello World</h1>
     </>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Box from '@material-ui/core/Box';
+
+export default function Nav() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Box display='flex' flexGrow={1}>
+          <Typography variant="h6" > News </Typography>
+        </Box>
+        <Button color="inherit" className="move-left">Login</Button>
+        <Button color="inherit" className="move-left">Sign up</Button>
+        {/* <IconButton edge="start"  color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton> */}
+      </Toolbar>
+    </AppBar>
+  );
+}
