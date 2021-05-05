a1680a326ac3aba315620694264d875e903eaf73
tien96ng <tien96ng@gmail.com>
2021-05-05 15:19:04 -0700

    Remove Public folder bloat files and add Container to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
+import Container from "@material-ui/core/Container"
 import '../css/App.css';
 
 
 function App() {
   return (
-    <>
+    <Container maxWidth="lg">
       <Nav />
       <h1>Hello World</h1>
-    </>
+    </Container>
   );
 }
 
