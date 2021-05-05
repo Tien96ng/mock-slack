082c83ba3585336c7c1fb66ce3e60f806880e357
tien96ng <tien96ng@gmail.com>
2021-05-05 15:34:07 -0700

    Create Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,15 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
 
 export default function SignUp() {
   return (
     <form autoComplete="off">
-      <h1> Sign up for Mock Slack! </h1>
+      <h1> Welcome back, Please login! </h1>
       <TextField id="outlined-basic" label="Outlined" variant="Email" />
       <TextField id="outlined-basic" label="Outlined" variant="Password" />
-      <Button type="submit" variant="contained" color="default" startIcon={ <AddIcon /> } />
+      <Button type="submit" variant="contained" color="default" startIcon={ <VpnKeyIcon /> } />
     </form>
   );
 }
